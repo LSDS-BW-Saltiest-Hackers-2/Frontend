2bfeda0ef9b13c05683fa7027213b0c2f736f77c
toricruzmendiola <mtoricruz@gmail.com>
2020-05-26 14:09:12 -0700

    identified what I need to do with saveComment action for a user. Added action

--- salty-hackers/src/action/appAction.js
+++ salty-hackers/src/action/appAction.js
@@ -1,8 +1,21 @@
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 export const SAVE_COMMENT_SUCCESS = 'SAVE_COMMENT_SUCCESS';
-export const SAVE_COMMENT_FAILUE = 'SAVE_COMMENT_SUCCESS';
-
+export const SAVE_COMMENT_FAILURE = 'SAVE_COMMENT_FAILURE';
+export const saveComment = savedComment => {
+    return dispatch => {
+        axiosWithAuth()
+            .post('URLOFAPIENDPOINTGOESHERE', savedComment)
+            .then(res => {
+                console.log('SAVED COMMENT SUCCEEDED: ', res)
+                // dispatch({ type: SAVE_COMMENT_SUCCESS, payload: res.data })
+            })
+            .catch(err => {
+                console.log(err)
+                dispatch({ type: SAVE_COMMENT_FAILURE, payload: console.log(err, '<----SAVE COMMENT FAILURE')})
+            })
+    }
+}
 
 export const FETCH_SAVED_COMMENTS_START = 'FETCH_SAVED_COMMENTS_START'; 
 export const FETCH_SAVED_COMMENTS_SUCCESS = 'FETCH_SAVED_COMMENTS_SSUCCESS'; 
