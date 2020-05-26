3c88143670171769bd7f796a9485410c32676d43
toricruzmendiola <mtoricruz@gmail.com>
2020-05-26 15:55:54 -0700

    implemented login

--- salty-hackers/src/components/Login/Login.js
+++ salty-hackers/src/components/Login/Login.js
@@ -20,16 +20,19 @@ class Login extends React.Component {
         });
     };
 
-    // login = e => {
-    //     e.preventDefault();
-    //     this.setState({ isLoading: true })
-    //     axiosWithAuth()
-    //         .post('PLACEHOLDERENDPOINT', res.data.payload)
-    //         .then(res => {
-    //             console.log(res)
-    //         })
-    //         .catch(err => console.log(err));
-    // }
+    login = e => {
+        e.preventDefault();
+        this.setState({ isLoading: true })
+        axiosWithAuth()
+            .post('/auth/login', this.state.credentials)
+            .then(res => {
+                console.log('USER LOGGED IN-->', res)
+                localStorage.setItem('token', res.data)
+                this.props.history.push('/main-feed')
+                this.setState({isLoading: false})
+            })
+            .catch(err => console.log(err));
+    }
 
     render() {
         const {isLoading} = this.state
